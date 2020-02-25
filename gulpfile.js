7f513f54493bbfbbb465c4cf306d767ae18cbe86
Sadulllah Zolfqar <sadullah.zolfqar@gmail.com>
2020-02-25 11:33:36 +0300

    add binary search tree

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
+    uglify = require('gulp-uglify');
+    minify  = require("gulp-babel-minify");
+   //require("gulp-babel-minify");
 
+
+
+gulp.task('scripts', function () {
+    return gulp.src([
+        'src/js/node.js',
+        'src/js/binar-search-tree.js',
+    ])
+    .pipe(minify({
+        mangle: {
+          keepClassName: true
+        }
+      }))
+    .pipe(concat('binar-search-tree.min.js'))
+    .pipe(gulp.dest('dist/bns'));
+});
 
